a22000fb9913742e226eccc4c90416d4efdba865
mariavass00 <126398989+mariavass00@users.noreply.github.com>
2023-05-22 15:39:35 +0200

    Update data-assessment.py

--- data-assessment.py
+++ data-assessment.py
@@ -22,13 +22,13 @@ extraTime = 0
 if st.button("Animate"):
 
     if mode == "Plotly":
+     
       df = pd.DataFrame(dict(
-      r=[1, 5, 2, 2, 3],
-      theta=['processing cost','mechanical properties','chemical stability',
+             r=[1, 5, 2, 2, 3],
+        theta=['processing cost','mechanical properties','chemical stability',
            'thermal stability', 'device integration']))
-      fig = px.line_polar(df, r='r', theta='theta', line_close=True)
-      fig.update_traces(fill='toself')
-      fig.show()
+        fig = px.line_polar(df, r='r', theta='theta', line_close=True)
+        fig.show()  
 
     if mode == "Altair":
         fig1 = alt.Chart(data).mark_point().encode(x="x",y="y")
